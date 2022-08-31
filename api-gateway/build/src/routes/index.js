33f89e4edab52900c5217f5ef9c5129fc4c65260
Mamun-swe <mamun.swe.277@gmail.com>
2022-08-31 13:42:43 +0600

    Auth service added to gateway

--- api-gateway/build/src/routes/index.js
+++ api-gateway/build/src/routes/index.js
@@ -3,6 +3,16 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.ROUTES = void 0;
 exports.ROUTES = [
     {
+        url: "/auth",
+        proxy: {
+            target: "http://localhost:5001/",
+            changeOrigin: true,
+            pathRewrite: {
+                [`^/auth`]: '',
+            }
+        }
+    },
+    {
         url: "/shopping-cart",
         proxy: {
             target: "http://localhost:5000/",
--- api-gateway/src/routes/index.ts
+++ api-gateway/src/routes/index.ts
@@ -3,6 +3,16 @@ import { IRoutes } from "../types"
 
 export const ROUTES: IRoutes[] = [
     {
+        url: "/auth",
+        proxy: {
+            target: "http://localhost:5001/",
+            changeOrigin: true,
+            pathRewrite: {
+                [`^/auth`]: '',
+            }
+        }
+    },
+    {
         url: "/shopping-cart",
         proxy: {
             target: "http://localhost:5000/",
